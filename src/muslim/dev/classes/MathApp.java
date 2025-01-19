94fcdbaa7e46212fed3cb63eb9ab8e20e9164699
herman-xphp <hermanxphp@gmail.com>
2025-01-19 23:01:37 +0800

    belajar math class

---
+++ src/muslim/dev/classes/MathApp.java
@@ -0,0 +1,13 @@
+public class MathApp {
+
+    public static void main(String[] args) {
+
+        var min = Math.min(12043, 88393);
+        System.out.println(min);
+
+        var max = Math.max(23232, 21232);
+        System.out.println(max);
+
+        System.out.println(Math.PI);
+    }
+}
